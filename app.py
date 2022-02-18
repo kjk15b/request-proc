af46e0251176455b7a3be6a7c8b5bd020d014410
dumbo <dumbo@baby.com>
2022-02-18 17:51:53 -0600

    Fix to correct read method

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import serial
 
 if __name__ == '__main__':
     conn = serial.Serial(sys.argv[1], baudrate=9600)
-    feedBack = conn.read()
+    feedBack = conn.readline()
     print("Found={}".format(feedBack))
     conn.close()
