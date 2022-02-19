39500b1a39f9cf6a1ffed9b94144ee3a4bf1d045
Kolby Kiesling <kjk15b@acu.edu>
2022-02-19 10:34:39 -0600

    documenting

--- lib/Common/USB/Driver.py
+++ lib/Common/USB/Driver.py
@@ -8,12 +8,21 @@ class Driver():
         except:
             print("Error connecting to serial device: {}".format(deviceID))
 
+    '''
+    readLine
+    read line from USB, decode as UTF-8 string
+    '''
     def readLine(self):
         try:
             return str(self.conn.readline().decode('utf-8'))
         except:
             print("Error reading line")
             return -1
+    
+    '''
+    closeConn
+    close the current connection
+    '''
     def closeConn(self):
         try:
             return self.conn.close()
