1448ca4492c50c24494d35e8949587e8df4ae825
Kolby Kiesling <kjk15b@acu.edu>
2022-02-19 10:12:28 -0600

    Updated script to remove built-in ref to host.

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     processor = Processor(sys.argv[1], host=sys.argv[2], port=sys.argv[3], upperLimit=sys.argv[4])
     while True :
         processor.process()
-        time.sleep(0.1)
+        time.sleep(0.3)
     processor.cleanUp()
--- boot.sh
+++ boot.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+# $1=arduino
+# $2=host
+# $3=port
+# $4=ds size
+
+
 ard=$(dmesg | grep tty | grep ACM | tr -s ' ' | cut -d ' ' -f 5 | tr -s ':' | cut -d ':' -f 1);
 fullArd="/dev/$ard" ;
 echo $fullArd ;
 
-python app.py $fullArd "web-dev" "5050" 5
+python app.py $fullArd $1 $2 $3 ;
 
 
